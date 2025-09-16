5c3eb203d7b7e224dfba70ae33ca351f7da62719
Sourav <souravkjha2007@gmail.com>
2025-09-17 01:21:34 +0530

    17 sept

--- projects/classico/lib/practise.dart
+++ projects/classico/lib/practise.dart
@@ -2,22 +2,38 @@
 import 'dart:io';
 
 void main(){
-  print("Hello, Dart!");
-  stdout.write("Enter your name: ");
-  String? name = stdin.readLineSync();
-  print("Welcome, $name!");
-  Human();
+  // print("Hello, Dart!");
+  // stdout.write("Enter your name: ");
+  // String? name = stdin.readLineSync();
+  // print("Welcome, $name!");
+  // Human();
+  //
+  // int? a;
+  // a = 10; //initialization
+  // print(a);
+  //
+  // a=7;
+  // print(a);
+  //
+  //
+  // String name2 ="Raman";
+  // print(name2);
 
-  int? a;
-  a = 10; //initialization
-  print(a);
-
-  a=7;
-  print(a);
 
 
-  String name2 ="Raman";
-  print(name2);
+  print('welcome to dart programming');
+  String name = "raman";
+
+  var subject = "flutter";
+  print(name);
+  print(subject);
+
+
+  dynamic section;
+  section = "A";
+  section = 10;
+  print(section);
+  print(section.runtimeType);
 }
 
 class Human{
